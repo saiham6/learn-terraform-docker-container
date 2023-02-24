3e94058142883f8199d3fffa2e23ee3ec42929aa
saiham6 <36539728+saiham6@users.noreply.github.com>
2023-02-24 00:43:37 +0000

    Fixed:incorrect port (#1)

--- Dockerfile
+++ Dockerfile
@@ -9,5 +9,6 @@ RUN  pip3 install -r requirements.txt --target "${LAMBDA_TASK_ROOT}"
 # Copy function code
 COPY app.py ${LAMBDA_TASK_ROOT}
 
+EXPOSE 8080
 # Set the CMD to your handler (could also be done as a parameter override outside of the Dockerfile)
 CMD [ "app.handler" ]
--- main.tf
+++ main.tf
@@ -9,25 +9,11 @@ terraform {
 
 provider "docker" {}
 
-# resource "docker_image" "nginx" {
-#   name         = "nginx:latest"
-#   keep_locally = false
-# }
-
-# resource "docker_container" "nginx" {
-#   image = docker_image.nginx.latest
-#   name  = "tutorial"
-#   ports {
-#     internal = 80
-#     external = 8000
-#   }
-# }
-
 resource "docker_container" "lamda_terraform" {
   image = "lamda_test"
   name = "lamda-terraform"
   ports {
-    internal = 8000
+    internal = 8080
     external = 9000
   }
 }
